90cd76a54f563ea2091c5916a784765568ec38d3
LucasBehrend <lucasbehrend2006@gmail.com>
2024-10-16 17:14:35 -0300

    server listen

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ import express from 'express';
 
 const app = express();
 app.use(express.json());
-
+const port = 3000;
 app.get("/", (req, res) => {
     res.send("Hello World!");
 })
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}/`);
+});
